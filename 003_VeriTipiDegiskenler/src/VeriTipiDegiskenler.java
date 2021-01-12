718f5bcaed6ff50aff63413a7ddf4381b7a6264b
siliconelabs <77060215+siliconelabs@users.noreply.github.com>
2021-01-12 14:58:38 +0300

    Add files via upload

---
+++ 003_VeriTipiDegiskenler/src/VeriTipiDegiskenler.java
@@ -0,0 +1,49 @@
+
+public class VeriTipiDegiskenler {
+
+    public static void main(String[] args) {
+
+        boolean cevap = false;
+        System.out.println(cevap);
+        cevap = true;
+        System.out.println(cevap);
+
+        byte yas = 100;
+        System.out.println(yas);
+        short rastgeleSayi = 5000;
+        System.out.println(rastgeleSayi);
+        int ayniRakamliSayi = 111111111;
+        System.out.println(ayniRakamliSayi);
+        long tckn = 11111111111L;
+        System.out.println(tckn);
+
+        float ondalikSayi = 10.5F;
+        System.out.println(ondalikSayi);
+        float ondalikSayi2 = 10;
+        System.out.println(ondalikSayi2);
+        double buyukOndalikliSayi = 2237238423748324742842837432454385345435342734.5D;
+        System.out.println(buyukOndalikliSayi);
+
+        char karakterDegiskeni = '5';
+        System.out.println(karakterDegiskeni);
+        
+        String kursAdi = "Kapsamli Java Eğitimi";
+        System.out.println(kursAdi);
+
+    }
+}
+
+/*----------------------------------------------------------------------------------------------------------------------
+	Temel türler (pre-defined, built-in):
+	Tür ismi			Uzunluğu (byte)
+	short					2
+   *int						4
+	long					8
+	byte					1
+
+   *double					8
+	float					4
+
+	char					2
+	boolean					-
+----------------------------------------------------------------------------------------------------------------------*/
